ae137e87d15586cfa1a3c31e1893340e1495180d
krylex <adam.mkh@hotmail.fr>
2023-03-02 17:59:55 +0100

    updated script function

--- k8s_postgres_install.sh
+++ k8s_postgres_install.sh
@@ -1,10 +1,29 @@
 . values
 
+
+# for i in 1
+# do
+#   if [ $i -eq 1 ]; then
+#       POSTGRES_NAMESPACE=postgres-master
+#       echo "PostgreSQL namespace: ${POSTGRES_NAMESPACE}"
+#   else
+#       POSTGRES_NAMESPACE=postgres-slave
+#       echo "PostgreSQL namespace: ${POSTGRES_NAMESPACE}"
+#   fi
+
+echo "Creation of namespace: ${POSTGRES_NAMESPACE}..."
+kubectl create namespace ${POSTGRES_NAMESPACE} --dry-run=client -o yaml | kubectl apply -f -
+
+
+echo "Installing PostgreSQL in namespace: ${POSTGRES_NAMESPACE}..."
+
 ## Creation of configMap
-echo 'apiVersion: v1
+echo '## Creation of configMap
+apiVersion: v1
 kind: ConfigMap
 metadata:
   name: postgres-config
+  namespace: '${POSTGRES_NAMESPACE}'
   labels:
     app: postgres
 data:
@@ -17,10 +36,12 @@ kubectl apply -f 01.postgres-configmap.yaml
 echo "ConfigMap applied successfully!"
 
 ## Creation of Persistent Storage Volume & Persistent Volume Claim
-echo 'kind: PersistentVolume
+echo '## Creation of Persistent Storage Volume & Persistent Volume Claim
 apiVersion: v1
+kind: PersistentVolume
 metadata:
   name: postgres-pv-volume
+  namespace: '${POSTGRES_NAMESPACE}'
   labels:
     type: local
     app: postgres
@@ -37,6 +58,7 @@ kind: PersistentVolumeClaim
 apiVersion: v1
 metadata:
   name: postgres-pv-claim
+  namespace: '${POSTGRES_NAMESPACE}'
   labels:
     app: postgres
 spec:
@@ -54,10 +76,12 @@ kubectl get pvc
 
 
 ## Creation of postgreSQL Deployment
-echo 'apiVersion: apps/v1
+echo '## Creation of postgreSQL Deployment
+apiVersion: apps/v1
 kind: Deployment
 metadata:
   name: postgres
+  namespace: '${POSTGRES_NAMESPACE}'
 spec:
   replicas: 1
   selector:
@@ -90,18 +114,20 @@ kubectl apply -f 03.postgres-deployment.yaml
 echo "PostgreSQL Deployment applied successfully!"
 
 ## Creation of PostgreSQL Service
-echo 'apiVersion: v1
+echo '## Creation of PostgreSQL Service
+apiVersion: v1
 kind: Service
 metadata:
   name: postgres
+  namespace: '${POSTGRES_NAMESPACE}'
   labels:
     app: postgres
 spec:
   type: NodePort
   ports:
-   - port: '${POSTGRES_PORT}'
+    - port: '${POSTGRES_PORT}'
   selector:
-   app: postgres' > 04.postgres-service.yaml
+    app: postgres' > 04.postgres-service.yaml
 
 echo "Applying PostgreSQL Service..."
 kubectl apply -f 04.postgres-service.yaml
@@ -110,19 +136,30 @@ echo "PostgreSQL Service applied successfully!"
 kubectl get all
 
 
-POSTGRES_POD_NAME=$(kubectl get pod -l app=postgres -o jsonpath="{.items[0].metadata.name}")
+
+
+
+
+
+
+
+
+POSTGRES_POD_NAME=$(kubectl get pod -n ${POSTGRES_NAMESPACE} -l app=postgres -o jsonpath="{.items[0].metadata.name}")
 
 echo "Waiting for PostgreSQL pod to be ready..."
-kubectl wait --for=condition=Ready pod/${POSTGRES_POD_NAME} --timeout=120s
+kubectl wait -n ${POSTGRES_NAMESPACE} --for=condition=Ready pod/${POSTGRES_POD_NAME} --timeout=120s
 echo "PostgreSQL pod is ready!"
 
+echo "PostgreSQL files are located in /var/lib/postgresql/data"
+
 ## Connection to PostgreSQL
-echo "Do you want to connect to PostgreSQL? (y/n) "
+echo "Do you want to enter the PostgreSQL pod? (y/n) "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    echo "Connecting to PostgreSQL..."
-    echo "kubectl exec -it ${POSTGRES_POD_NAME} --  psql -h localhost -U ${POSTGRES_USER} --password -p ${POSTGRES_PORT} ${POSTGRES_DB}"
-    kubectl exec -it ${POSTGRES_POD_NAME} --  psql -h localhost -U ${POSTGRES_USER} --password -p ${POSTGRES_PORT} ${POSTGRES_DB}
+    # kubectl exec -it -n ${POSTGRES_NAMESPACE} pod/${POSTGRES_POD_NAME} --  psql -h localhost -U ${POSTGRES_USER} --password -p ${POSTGRES_PORT} ${POSTGRES_DB}
+    echo "Connecting to PostgreSQL in namespace ${POSTGRES_NAMESPACE}..."
+    echo "kubectl exec -it -n ${POSTGRES_NAMESPACE} pod/${POSTGRES_POD_NAME} -- bash"
+    kubectl exec -it -n ${POSTGRES_NAMESPACE} pod/${POSTGRES_POD_NAME} -- bash
 else
     echo "Exiting..."
     exit 0
--- k8s_postgres_uninstall.sh
+++ k8s_postgres_uninstall.sh
@@ -1,10 +1,15 @@
-kubectl delete service postgres
+. values
 
-kubectl delete deployment postgres
+kubectl delete service -n ${POSTGRES_NAMESPACE} postgres
 
-kubectl patch pv postgres-pv-volume -p '{"metadata":{"finalizers":null}}'
-kubectl patch pvc postgres-pv-claim -p '{"metadata":{"finalizers":null}}'
-kubectl delete pvc postgres-pv-claim
-kubectl delete pv postgres-pv-volume
+kubectl delete deployment -n ${POSTGRES_NAMESPACE} postgres
 
-kubectl delete configmap postgres-config
+kubectl patch pv -n ${POSTGRES_NAMESPACE} postgres-pv-volume -p '{"metadata":{"finalizers":null}}'
+kubectl patch pvc -n ${POSTGRES_NAMESPACE} postgres-pv-claim -p '{"metadata":{"finalizers":null}}'
+kubectl delete pvc -n ${POSTGRES_NAMESPACE} postgres-pv-claim
+kubectl delete pv -n ${POSTGRES_NAMESPACE} postgres-pv-volume
+
+kubectl delete configmap -n ${POSTGRES_NAMESPACE} postgres-config
+
+kubectl delete namespace ${POSTGRES_NAMESPACE}
+# kubectl delete namespace postgres-slave
